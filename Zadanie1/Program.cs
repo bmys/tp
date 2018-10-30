43f62c4a138713fc404dbc3f67f0f25d4b2596ca
Mateusz <mi.si.ak.m996@gmail.com>
2018-10-30 18:27:24 +0100

    Empty proejct

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Mateusz Misiak
+//Bartosz Myśliwiec
+//30.10.2018
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
