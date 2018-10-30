5a9ad49ddc283c978f99d5dcaaa8fd884ee7e9a6
Mateusz <mi.si.ak.m996@gmail.com>
2018-10-30 19:00:55 +0100

    Utworzenie biblioteki klas, dodanie klasy klient oraz pojazd

---
+++ ClassLibrary/Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Class client represents people who rent vehicle
+/// </summary>
+namespace ClassLibrary
+{
+    public class Client
+    {
+        private string pesel;
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public string Pesel { get => pesel; set => pesel = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Age { get => age; set => age = value; }
+
+        public override bool Equals(object obj)
+        {
+            var client = obj as Client;
+            return client != null &&
+                   pesel == client.pesel &&
+                   firstName == client.firstName &&
+                   lastName == client.lastName &&
+                   age == client.age;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1795900458;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(pesel);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(firstName);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(lastName);
+            hashCode = hashCode * -1521134295 + age.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return "First Name: " + this.FirstName + " Last Name: " + this.LastName + " Pesel: " + this.Pesel + " Age: " + this.Age;
+        }
+
+    }
+}
---
+++ ClassLibrary/Vehicle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Class vehicle represents vehicle in the rental
+/// </summary>
+namespace ClassLibrary
+{
+    class Vehicle
+    {
+        private string registration;
+        private string vehicleBrand;
+        private string vehicleModel;
+        private int rentalPrice;
+
+        public string Registration { get => registration; set => registration = value; }
+        public string VehicleBrand { get => vehicleBrand; set => vehicleBrand = value; }
+        public string VehicleModel { get => vehicleModel; set => vehicleModel = value; }
+        public int RentalPrice { get => rentalPrice; set => rentalPrice = value; }
+
+        public override bool Equals(object obj)
+        {
+            var vehicle = obj as Vehicle;
+            return vehicle != null &&
+                   registration == vehicle.registration &&
+                   vehicleBrand == vehicle.vehicleBrand &&
+                   vehicleModel == vehicle.vehicleModel &&
+                   rentalPrice == vehicle.rentalPrice;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1381312012;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(registration);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(vehicleBrand);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(vehicleModel);
+            hashCode = hashCode * -1521134295 + rentalPrice.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return "Vehicle board: " + this.VehicleBrand + "Vehicle model: " + this.VehicleModel + 
+                   " Registration: " + this.Registration + "Rental price: " + this.RentalPrice;
+        }
+    }
+}
